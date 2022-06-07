42fe508f1534aa039648edfea93015237a097be1
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-06-07 20:17:50 +0200

    Create AuthorViewModel.cs

---
+++ Nick.DesignPatterns.mobile/ViewModels/AuthorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DesignPatterns.ViewModels
+{
+    public partial class AuthorViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string position = "Software Developer";
+
+        [ObservableProperty]
+        public string fullNames = "Nicolas Maluleke";
+
+        [ObservableProperty]
+        public static string bio =
+            @"
+            I am a highly result-driven developer, aspiring software architect, and a computer science student.
+            I am focused on expanding my knowledge and my abilities in programming and quality assurance.
+            I enjoy writing poetic and expressive code that speaks to both developers and domain experts of any
+            level and creating simplified software to enhance user interaction and experience.
+            ";
+    }
+}
+
