46dc6f3a8acb48e7960298dc2f7a08c8a1d793ac
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-06-10 00:15:33 +0200

    Update AboutPageViewModel.cs

--- Nick.DesignPatterns.mobile/ViewModels/AboutPageViewModel.cs
+++ Nick.DesignPatterns.mobile/ViewModels/AboutPageViewModel.cs
@@ -2,44 +2,36 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
-namespace DesignPatterns.ViewModels
+namespace DesignPatterns.ViewModels;
+
+public enum LinkType
 {
-    public enum LinkType
+    Github,
+    LinkedIn,
+    Twitter,
+    Website
+}
+public partial class AboutPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title = "About Design Patterns";
+
+    [ObservableProperty]
+    private AuthorViewModel author;
+
+    [RelayCommand]
+    public async Task OpenLinkAsync(LinkType linkType)
     {
-        Github,
-        LinkedIn,
-        Twitter,
-        Website
+        var execDict = new Dictionary<LinkType, Action>();
+
+        execDict.Add(LinkType.Github, async () => await OpenWebAsync("https://github.com/hnicolus"));
+        execDict.Add(LinkType.LinkedIn, async () => await OpenWebAsync("https://www.linkedin.com/in/nicolas-maluleke-81a698191"));
+        execDict.Add(LinkType.Twitter, async () => await OpenWebAsync("https://twitter.com/HNicolus"));
+        execDict.Add(LinkType.Website, async () => await OpenWebAsync("https://nicksoftware.co.za"));
+
+        await MainThread.InvokeOnMainThreadAsync(execDict[linkType]);
     }
-    public partial class AboutPageViewModel : ObservableObject
-    {
-        [ObservableProperty]
-        private string _title = "About Design Patterns";
 
-        [ObservableProperty]
-        private AuthorViewModel author;
-
-        [ICommand]
-        public async Task OpenLinkAsync(LinkType linkType)
-        {
-            var execDict = new Dictionary<LinkType, Action>();
-
-            execDict.Add(LinkType.Github, () => OpenWebAsync("https://github.com/hnicolus"));
-            execDict.Add(LinkType.LinkedIn, () => OpenWebAsync("https://www.linkedin.com/in/nicolas-maluleke-81a698191"));
-            execDict.Add(LinkType.Twitter, () => OpenWebAsync("https://twitter.com/HNicolus"));
-            execDict.Add(LinkType.Website, () => OpenWebAsync("https://nicksoftware.co.za"));
-
-            await MainThread.InvokeOnMainThreadAsync( () =>  execDict[linkType]());
-        }
-
-        private async Task OpenWebAsync(string url)
-        {
-            await Launcher.OpenAsync(url);
-        }
-
-        public AboutPageViewModel()
-        {
-        }
-    }
+    private async Task OpenWebAsync(string url) => Launcher.OpenAsync(url);
 }
 
