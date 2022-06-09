405bfc89c973c5c43392735bd2c90a52fefd0857
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-06-09 23:55:45 +0200

    moved interface to its own file.

--- Nick.DesignPatterns.mobile/Services/DesignPatternsService.cs
+++ Nick.DesignPatterns.mobile/Services/DesignPatternsService.cs
@@ -4,13 +4,6 @@ using DesignPatterns.Models;
 
 namespace DesignPatterns.Services
 {
-    public interface IDesignPatternsService
-    {
-        Task<List<Category>> GetCategoriesAsync();
-        Task<List<Pattern>> GetCategoryPatternsAsync(int categoryId);
-        Task<Category> GetCategoryAsync(int categoryId);
-        Task<Pattern> GetPatternAsync(int patternId);
-    }
 
     public class DesignPatternsService : IDesignPatternsService
     {
---
+++ Nick.DesignPatterns.mobile/Services/IDesignPatternsService.cs
@@ -0,0 +1,13 @@
+using DesignPatterns.Models;
+
+namespace DesignPatterns.Services
+{
+    public interface IDesignPatternsService
+    {
+        Task<List<Category>> GetCategoriesAsync();
+        Task<List<Pattern>> GetCategoryPatternsAsync(int categoryId);
+        Task<Category> GetCategoryAsync(int categoryId);
+        Task<Pattern> GetPatternAsync(int patternId);
+    }
+}
+
