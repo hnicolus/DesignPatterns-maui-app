1242f6e0574e98529b468f0949d33b245c8c389d
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-06-10 00:15:06 +0200

    used inline namespace.

--- Nick.DesignPatterns.mobile/Services/IDesignPatternsService.cs
+++ Nick.DesignPatterns.mobile/Services/IDesignPatternsService.cs
@@ -1,13 +1,11 @@
 using DesignPatterns.Models;
 
-namespace DesignPatterns.Services
-{
-    public interface IDesignPatternsService
-    {
-        Task<List<Category>> GetCategoriesAsync();
-        Task<List<Pattern>> GetCategoryPatternsAsync(int categoryId);
-        Task<Category> GetCategoryAsync(int categoryId);
-        Task<Pattern> GetPatternAsync(int patternId);
-    }
-}
+namespace DesignPatterns.Services;
 
+public interface IDesignPatternsService
+{
+    Task<List<Category>> GetCategoriesAsync();
+    Task<List<Pattern>> GetCategoryPatternsAsync(int categoryId);
+    Task<Category> GetCategoryAsync(int categoryId);
+    Task<Pattern> GetPatternAsync(int patternId);
+}
