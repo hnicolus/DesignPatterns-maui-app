51eae2243be068dd0d91fd20c0710d80ffa52ef8
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-06-10 00:15:21 +0200

    Update PatternDetailPageViewModel.cs

--- Nick.DesignPatterns.mobile/ViewModels/PatternDetailPageViewModel.cs
+++ Nick.DesignPatterns.mobile/ViewModels/PatternDetailPageViewModel.cs
@@ -3,24 +3,23 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using DesignPatterns.Models;
 using DesignPatterns.Services;
 
-namespace DesignPatterns.ViewModels
+namespace DesignPatterns.ViewModels;
+
+public partial class PatternDetailPageViewModel : ObservableObject, IQueryAttributable
 {
-	public partial class PatternDetailPageViewModel : ObservableObject, IQueryAttributable
-	{
-        private readonly IDesignPatternsService _designPatternsService;
+    private readonly IDesignPatternsService _designPatternsService;
 
-        [ObservableProperty]
-        private Pattern _pattern;
+    [ObservableProperty]
+    private Pattern _pattern;
 
-		public PatternDetailPageViewModel(IDesignPatternsService designPatternsService)
-		{
-            _designPatternsService = designPatternsService;
-        }
+    public PatternDetailPageViewModel(IDesignPatternsService designPatternsService)
+    {
+        _designPatternsService = designPatternsService;
+    }
 
-        public void ApplyQueryAttributes(IDictionary<string, object> query)
-        {
-            Pattern = query[nameof(Pattern)] as Pattern;
-        }
-	}
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        Pattern = query[nameof(Pattern)] as Pattern;
+    }
 }
 
