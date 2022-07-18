835cb28ac0f24e64a84e91324159ef2678184c56
Nicolas Maluleke <nicolusmaluleke@gmail.com>
2022-07-18 10:24:50 +0200

    Update MauiProgram.cs

--- Nick.DesignPatterns.mobile/MauiProgram.cs
+++ Nick.DesignPatterns.mobile/MauiProgram.cs
@@ -17,8 +17,11 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+        
         builder.Services.AddBlazorWebView();
+        
         builder.ConfigureServices();
+        
         return builder.Build();
     }
 }
