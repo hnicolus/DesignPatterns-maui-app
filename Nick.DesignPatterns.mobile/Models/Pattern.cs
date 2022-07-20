83b3d4eaa4c8d0d8fab21b7df73fd19166a2cf44
Nicolas <nicolusmaluleke@gmail.com>
2022-07-21 00:18:57 +0200

    Removed unused property.

--- Nick.DesignPatterns.mobile/Models/Pattern.cs
+++ Nick.DesignPatterns.mobile/Models/Pattern.cs
@@ -6,7 +6,5 @@ public class Pattern
     public int CategoryId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-
-    public List<string> Images { get; set; }
     public string Icon { get; set; }
 }
