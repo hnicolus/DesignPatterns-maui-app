943069909fe4a9e76236ce478c5d3edf1bcbd9c1
Nicolas <36539498+hnicolus@users.noreply.github.com>
2022-11-07 01:50:01 +0200

    Updated nuget packages and cleaned up warnings. (#6)
    * Moved to its own file
    
    * Removed inline interfaces.
    
    * Formatted csproj.
    
    * Removed unused property.
    
    * Removed MAUI dependency references (MAUI does has an implicit referencing mechanism built in)
    
    This was causing warning to appear.
    
    * Update AppShell.xaml
    
    * Update AuthorViewModel.cs
    
    * Optimised collection sizing.
    
    * Create global.json
    
    * Update: CICD pipeline (only install required workloads for each platform.)
    
    * Removed globaljson file.
    
    * Fixed pipeline.
    
    * Changed build dotnet version.
    
    * restored old CICD workflow.
    
    * Disable windows build

--- Nick.DesignPatterns.mobile/ViewModels/AuthorViewModel.cs
+++ Nick.DesignPatterns.mobile/ViewModels/AuthorViewModel.cs
@@ -5,11 +5,9 @@ namespace DesignPatterns.ViewModels;
 public partial class AuthorViewModel : ObservableObject, ITransientDependency
 {
     [ObservableProperty] private string bio =
-        @"
-            I am just a dude who writes a lot of code.
-            I enjoy writing poetic and expressive code that speaks to both developers and domain experts of any
-            level and creating simplified software to enhance user interaction and experience.
-            ";
+        @"I am just a dude who writes a lot of code.
+        I enjoy writing poetic and expressive code that speaks to both developers and domain experts of any
+        level and creating simplified software to enhance user interaction and experience.";
 
     [ObservableProperty] private string fullNames = "Nicolas Maluleke";
     [ObservableProperty] private string position = "Software Developer";
